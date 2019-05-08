7a548ab3fb94a2922f7a3c3e3f61322f77031476
lajawanti <myemail>
2019-05-08 13:31:41 -0400

    custom middleware added but req.query.. not working

--- api/server.js
+++ api/server.js
@@ -12,11 +12,25 @@ const morgan = require("morgan")
 server.use(express.json()); //built-in middleware
 server.use(morgan('short'));//morgan('dev') // third-party middleware give all info about request received .. so if needed can be saved to database
 
+//adding custom middleware
+function upper(req, res, next) {
+    console.log(req.query.name);
+    //You can access query from URL passed with '?' like ...http://localhost:5000/name/?name = hello
+    const nameRecived = req.query.name || 'this is a test';
+    req.upper = nameRecived.toUpperCase();
+    console.log(req.upper);
+    next();
+}
+server.use(upper);
 
 //routes... to give response according to request received 
 server.get('/', (req, res) => {
     res.send("SERVER  LIVE........");
     //can use express.static to send static file with all impages and css...
+})
+
+server.get('/name', (req, res) => {
+    res.send(`the name received is ${req.query.name} and after custom middleware it is ${req.upper}`);
 })
 
 //need to export objects and functions so other file can import and use them
